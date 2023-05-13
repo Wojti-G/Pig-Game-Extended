3cee9aae93bf23217f31dccf10a4d936784a25c4
Wojti-G <130251605+Wojti-G@users.noreply.github.com>
2023-05-13 14:16:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-'use strict';
+"use strict";
 
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
-const dice0El = document.querySelector('.dice--0');
-const dice1El = document.querySelector('.dice--1');
+const dice0El = document.querySelector(".dice--0");
+const dice1El = document.querySelector(".dice--1");
 
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
-const name0El = document.getElementById('name--0');
-const name1El = document.getElementById('name--1');
+const name0El = document.getElementById("name--0");
+const name1El = document.getElementById("name--1");
 
 // --------------------------------------------------------
 // Dodatek:
 // - 2 kości w grze
 // - jeśli wypadną 2 takie same kości to punkty x2
 // - jeśli którakolwiek z kości to 1 -> switchPlayer
+
+// - zdobądź punkty w okolicach pełnych setek by wygrać. Kolejna setka to przedział większy o 1.
 // --------------------------------------------------------
 
 // Starting conditions
@@ -35,33 +37,33 @@ const init = function () {
   activePlayer = 0;
   playing = true;
 
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 
   score0El.textContent = 0;
   score1El.textContent = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
-  name0El.textContent = 'PLAYER 1';
-  name1El.textContent = 'PLAYER 2';
+  name0El.textContent = "PLAYER 1";
+  name1El.textContent = "PLAYER 2";
 
-  dice0El.classList.add('hidden');
-  dice1El.classList.add('hidden');
+  dice0El.classList.add("hidden");
+  dice1El.classList.add("hidden");
 };
 init();
 
 // Animate rolls
 const spinning0 = [
-  { transform: 'translateX(-50%) rotate(0) scale(1)' },
-  { transform: 'translateX(-50%) rotate(90deg) scale(1.25)' },
-  { transform: 'translateX(-50%) rotate(180deg) scale(1)' },
+  { transform: "translateX(-50%) rotate(0) scale(1)" },
+  { transform: "translateX(-50%) rotate(90deg) scale(1.25)" },
+  { transform: "translateX(-50%) rotate(180deg) scale(1)" },
 ];
 const spinning1 = [
-  { transform: 'translateX(-50%) rotate(0) scale(1)' },
-  { transform: 'translateX(-50%) rotate(-90deg) scale(1.25)' },
-  { transform: 'translateX(-50%) rotate(-180deg) scale(1)' },
+  { transform: "translateX(-50%) rotate(0) scale(1)" },
+  { transform: "translateX(-50%) rotate(-90deg) scale(1.25)" },
+  { transform: "translateX(-50%) rotate(-180deg) scale(1)" },
 ];
 
 // Switch player
@@ -69,15 +71,15 @@ const switchPlayer = function () {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 // Player rolls a dice
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
-    dice0El.classList.remove('hidden');
-    dice1El.classList.remove('hidden');
+    dice0El.classList.remove("hidden");
+    dice1El.classList.remove("hidden");
 
     let roll0 = Math.trunc(Math.random() * 6 + 1);
     let roll1 = Math.trunc(Math.random() * 6 + 1);
@@ -106,7 +108,7 @@ btnRoll.addEventListener('click', function () {
 });
 
 // Player holds their score
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
@@ -122,21 +124,21 @@ btnHold.addEventListener('click', function () {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
-      document.getElementById(`name--${activePlayer}`).textContent = 'WINNER!';
+        .classList.add("player--winner");
+      document.getElementById(`name--${activePlayer}`).textContent = "WINNER!";
     } else {
       if (scores[activePlayer] > 505) {
         switchPlayer();
         playing = false;
         document
           .querySelector(`.player--${activePlayer}`)
-          .classList.add('player--winner');
+          .classList.add("player--winner");
         document.getElementById(`name--${activePlayer}`).textContent =
-          'WINNER!';
+          "WINNER!";
       } else switchPlayer();
     }
   }
 });
 
 // Player resets game
-btnNew.addEventListener('click', init);
+btnNew.addEventListener("click", init);
